84a47fc32201ceae0290717def021013a4751909
karan <karanpatel2329@gmail.com>
2024-09-21 15:56:42 +0530

    chatgpt

--- api/index.js
+++ api/index.js
@@ -1,21 +1,16 @@
 
-import http from 'http';
- 
-// Create a server object
-const server = http.createServer((req, res) => {
-    // Set the response header
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    // Write some text to the response
-    res.end('Welcome to my simple Node.js app!');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel!');
 });
- 
-// Define the port to listen on
-const port = 3000;
- 
-// Start the server
-server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello, world!' });
 });
+
+module.exports = app;
 // const crypto = require('crypto');
 // const express = require('express');
 // const app = express();
